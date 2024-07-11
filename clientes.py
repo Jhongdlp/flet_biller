435089e330849361a9ae5641f55c298c6530a248
Jhongdlp <jhondlp204@gmail.com>
2024-07-11 12:50:46 -0500

    clientes avabaco interface

--- clientes.py
+++ clientes.py
@@ -2,7 +2,7 @@ import flet as ft
 from components.rail import create_navigation_rail
 
 def main(page: ft.page):
-    pagina=ft.Text("Funciona")
+
     Contenedor_rail =ft.Container(padding=0,
         width=140,
         height=650,
@@ -31,7 +31,40 @@ def main(page: ft.page):
         icon=ft.icons.LIGHT_MODE,  # Icono inicial del sol
         on_click=toggle_theme
     )
-
+    cedula_campo_texfild=ft.TextField(hint_text="Cedula",
+        content_padding=5,
+        width=175,  
+        height=35,           
+    )
+    nombre_cliente_texfield=ft.TextField(hint_text="Nombre del cliente",
+        content_padding=5,
+        width=175,  
+        height=35,           
+    )   
+    apellido_cliente_texfield=ft.TextField(hint_text="Apellido del cliente",
+        content_padding=5,
+        width=175,
+        height=35
+    )
+    direccion_cliente_texfield=ft.TextField(hint_text="Direccion",
+        content_padding=5,
+        min_lines=4, # Controla la altura visible
+        max_lines=4,
+        #height=100, # Define la altura fija
+        width=360,
+        max_length=150,  
+        #height=35,           
+    )
+    telefono_cliente_texfield=ft.TextField(hint_text="Telefono del cliente",
+        content_padding=5,
+        width=115,  
+        height=35,     
+    )
+    mail_cliente_texfield=ft.TextField(hint_text="Mail del cliente",
+        content_padding=5,
+        width=240,
+        height=35
+    )
     view_clientes=ft.Column([
         ft.Container(
             padding=0,
@@ -94,56 +127,56 @@ def main(page: ft.page):
                                             alignment=ft.alignment.center,
                                             padding=10,
                                             content=ft.Column([
+                                                ft.Container(
+                                                    alignment=ft.alignment.center_left,
+                                                    content=ft.Row([
+                                                        ft.Column([
+                                                            ft.Text("Identificador:",weight=ft.FontWeight.W_900,size=14),
+                                                            ft.Row([
+                                                                cedula_campo_texfild,
+                                                                ft.IconButton(icon=ft.icons.DOCUMENT_SCANNER_ROUNDED),
+                                                                ft.IconButton(icon=ft.icons.FEATURED_VIDEO_OUTLINED),
+                                                            ]),         
+                                                            
+                                                        ]),
+                                                ]),
+                                                ),
                                                 ft.Row([
                                                     ft.Column([
                                                         ft.Text("Nombre:",weight=ft.FontWeight.W_900,size=14),
-                                                        ft.TextField(hint_text="Nombre del cliente",
-                                                            content_padding=5,
-                                                            width=175,  
-                                                            height=35,           
-                                                        )
+                                                        nombre_cliente_texfield
                                                         
                                                     ]),
                                                     ft.Column([
                                                         ft.Text("Apellido:",weight=ft.FontWeight.W_900,size=14),
-                                                        
-                                                        ft.TextField(hint_text="Apellido del cliente",
-                                                            content_padding=5,
-                                                            width=175,
-                                                            height=35
-                                                        )
+                                                        apellido_cliente_texfield
                                                     ]),
 
                                                 ]),
                                                 ft.Column([
                                                     ft.Text("Direccion:",weight=ft.FontWeight.W_900,size=14),
-                                                    ft.TextField(hint_text="Direccion",
-                                                        content_padding=5,
-                                                        width=175,  
-                                                        height=35,           
-                                                    )
+                                                    direccion_cliente_texfield
                                                     
                                                 ]),
                                                 ft.Row([
                                                     ft.Column([
                                                         ft.Text("Telefono:",weight=ft.FontWeight.W_900,size=14),
-                                                        ft.TextField(hint_text="Telefono del cliente",
-                                                            content_padding=5,
-                                                            width=115,  
-                                                            height=35,     
-                                                        )
+                                                        telefono_cliente_texfield
                                                         
                                                     ]),
                                                     ft.Column([
                                                         ft.Text("mail:",weight=ft.FontWeight.W_900,size=14),
-                                                        
-                                                        ft.TextField(hint_text="Mail del cliente",
-                                                            content_padding=5,
-                                                            width=240,
-                                                            height=35
-                                                        )
+                                                        mail_cliente_texfield
                                                     ])
                                                 ]),
+                                                ft.Divider(height=3),
+                                                ft.Container(
+                                                    alignment=ft.alignment.top_right,
+                                                    content=ft.Row([
+                                                        ft.ElevatedButton("Limpiar",bgcolor=ft.colors.RED_600,color=ft.colors.WHITE),
+                                                        ft.ElevatedButton("Guardar cliente",bgcolor=ft.colors.GREEN_600,color=ft.colors.WHITE), 
+                                                    ],ft.MainAxisAlignment.END)
+                                                )
                                                 
                                             ])            
                                         ),
@@ -154,17 +187,32 @@ def main(page: ft.page):
                         ft.Container(width=800,height=600,
                             #border=ft.border.all(),
                             alignment=ft.alignment.center,
-                            content=ft.Container(width=795,height=595,
-                                #border=ft.border.all(),
-                                padding=10,
-                                content=ft.Container(ft.Text("ola"))                 
-                            )
+                            content=ft.Column([
+                                ft.Container(width=795,
+                                    #height=50,
+                                    border=ft.border.all(),
+                                    padding=10,
+                                    content=ft.Container(
+                                        ft.Column([
+                                            ft.Text("Busca a un cliente:",weight=ft.FontWeight.W_900,size=25),
+                                            ft.Row([
+                                                ft.TextField(label="Bucar por cedula",width=250),
+                                                ft.TextField(label="Bucar por RUC",width=250),
+                                                ft.TextField(label="Bucar por pasaporte",width=250),
+                                            ])
+                                        ]),
+                                        
+                                    )                 
+                                ),
+                                ft.Container(width=795
+                                    #!PONER AQUI EL DATATABLE DE LA GENERACION DE LA BUSQUEDA
+                                )
+                            ])
                         ),
                     ])
 
                 )
             ],alignment=ft.MainAxisAlignment.START)
-
         )
         ]
     )
